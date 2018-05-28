bc3583c70ac3aa4bab577864b31f56b4eb576cf5
francisco-garciam <francisco.garciamunizaga@hotmail.com>
2018-05-28 16:44:12 -0400

    Se agrega función que muestra todos los tags de cada tópico

--- React-PMV/src/ListaTopicos.js
+++ React-PMV/src/ListaTopicos.js
@@ -8,6 +8,19 @@ class ListaTopicos extends Component {
     topicos: []
   };
 
+  tags(keyword_topic) {
+    var keys = "";
+    for (var j = 0; j < keyword_topic.length; j++){
+        if(j  == keyword_topic.length -1){
+            keys = keys.concat(keyword_topic[j].name);
+        }
+        else{
+            keys = keys.concat(keyword_topic[j].name + ", ");
+        }
+    }
+    return keys;
+  }
+
   async componentDidMount() {
     try {
       const res = await fetch('http://127.0.0.1:8000/topic/');
@@ -22,9 +35,9 @@ class ListaTopicos extends Component {
 
   render() {
     return (
-      
-    <form> 
-    <table className="table table-hover"> 
+
+    <form>
+    <table className="table table-hover">
       <thead>
             <tr>
                 <th>Nombre</th>
@@ -37,17 +50,17 @@ class ListaTopicos extends Component {
 
             <tr>
                 <td>{item.name}</td>
-                <td>Python, Desarrollo</td>
+                <td>{this.tags(item.keyword_topic)}</td>
                 <td ><input type="checkbox" name={item.id} id={item.id} className="css-checkbox" value={item.id} /><label for={item.id} className="css-label"></label></td>
             </tr>
-          
+
      ))}
     </tbody>
     </table>
        <button type="button" className="btn btn-primary">Guardar Cambios</button>
 
    </form>
-      
+
     );
   }
 }
