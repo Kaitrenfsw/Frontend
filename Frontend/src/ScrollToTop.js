0053be282534257b4d3a1d296bd690c558ba52bb
francisco-garciam <francisco.garciamunizaga@hotmail.com>
2018-11-26 13:04:48 -0300

    Fixed detalle-tema mobile versión.

--- Frontend/src/ScrollToTop.js
+++ Frontend/src/ScrollToTop.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, Redirect,withRouter} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
